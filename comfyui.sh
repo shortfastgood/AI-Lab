7b2ef3f97cec431df370286cc001e2f196f43b42
Daniele Denti <denti.daniele@gmail.com>
2026-07-30 16:34:50 +0200

    fix: ComfyUI startup script

--- comfyui.sh
+++ comfyui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/gitroot/ComfyUI
 source venv/bin/activate
-export HF_HOME="$HOME/ComfyUI/.cache/huggingface"
+export HF_HOME="$HOME/gitrootComfyUI/.cache/huggingface"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 python main.py
