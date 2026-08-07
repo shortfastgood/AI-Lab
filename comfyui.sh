9b33850d7955024af3d2ea1be302381723bba5aa
Daniele Denti <denti.daniele@gmail.com>
2026-08-07 11:04:54 +0200

    script: enable manager

--- comfyui.sh
+++ comfyui.sh
@@ -3,4 +3,4 @@ cd ~/gitroot/ComfyUI
 source venv/bin/activate
 export HF_HOME="$HOME/gitrootComfyUI/.cache/huggingface"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
-python main.py
+python main.py --enable-manager
